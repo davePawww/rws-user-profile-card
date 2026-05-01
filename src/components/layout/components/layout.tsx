0e854ca9e8f1186877ed1a78bcf3462d7ffe9201
DavePaurillo <paurillo.dave@gmail.com>
2026-05-01 18:07:14 +0800

    styles: added custom scrollbar | updated the main layout | updated the animation timing

--- src/components/layout/components/layout.tsx
+++ src/components/layout/components/layout.tsx
@@ -9,13 +9,16 @@ export default function Layout({ children }: LayoutProps) {
   useThemeSync();
 
   return (
-    <div className="mx-auto flex min-h-dvh max-w-6xl flex-col overflow-hidden p-4">
-      <Header title="03-user-profile-card" projectLink="https://github.com/davePawww/rws-user-profile-card" />
+    <div className="mx-auto flex h-dvh max-w-6xl flex-col overflow-hidden p-4">
+      <Header
+        title="03-user-profile-card"
+        projectLink="https://github.com/davePawww/rws-user-profile-card"
+      />
       <motion.main
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1, ease: 'backIn' }}
-        className="full-shadow my-4 flex flex-1 items-center justify-center rounded-lg border p-4"
+        className="full-shadow my-4 flex flex-1 flex-col items-center justify-center overflow-y-auto rounded-lg border p-4"
       >
         {children}
       </motion.main>
